f8fafd77437f0ad9df5a32b7af5dab27ae9c6597
Sergei_Sharko <vulfer_d@mail.ru>
2024-09-09 10:13:16 +0300

    Добавьте файлы проекта.

---
+++ Message.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ChatApp
+{
+    internal class Message
+    {
+        public string? FromName { get; set; }
+        public DateTime Date { get; set; }
+        public string? Text { get; set; }
+
+        // Метод для сериализации в JSON
+        public string ToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+        // Статический метод для десериализации JSON в объект Message
+        public static Message? FromJson(string json)
+        {
+            return JsonSerializer.Deserialize<Message>(json);
+        }
+
+    }
+}
